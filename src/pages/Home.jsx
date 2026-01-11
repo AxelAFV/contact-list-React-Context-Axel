81a6ceed14b393776cafd61551ab6b5308d870d8
AxelAFV <axel.fernandezvalverde@gmail.com>
2026-01-11 18:53:47 +0000

    comentarios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,8 @@ export const Home = () => {
 	useEffect(()=>{
 		getContacts(dispatch);
 	},[]);
-
+	// el error en consola de tabIndex es un error de typeo de Id con react creo. Pero para el modal en especifico al cambiar todos los "id" por tabIndex no me funciono el modal.
+	// Otro error que consegui es que al inicio cuando la Lista esta vacia, al crear el primer contacto se duplica, estuve buscando la razon y no logre dar con ello, IM SO SORRY!
 	return (
 		<div className="container mt-5">
 				{store.contacts.map(contact => {
